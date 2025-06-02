f6498e55d41afa9cfd23127767254c0fe70c71fe
projectssl <projects@steph-marketing.com>
2025-06-02 16:22:02 +1000

    Update client.js

--- client.js
+++ client.js
@@ -4,13 +4,15 @@ window.TrelloPowerUp.initialize({
   'board-buttons': function(t) {
     return [{
       text: 'Test Button',
-      callback: function(t) {
-        console.log("✅ Test button clicked");
-        return t.popup({
-          title: "Popup Title",
-          url: "index.html"
-        });
-      }
+      callback: 'showAlert'
     }];
+  },
+  'showAlert': function(t) {
+    console.log("✅ Test button clicked");
+    return t.popup({
+      title: "Popup Title",
+      url: "./index.html"
+    });
   }
 });
+
